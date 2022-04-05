fefd5e5de694c6575e617a30592a1fb118606163
Nat Allan <nat@natallan.com>
2022-04-05 23:33:22 +1000

    docs: cleanup diagrams.py

--- docs/src/images/cluster-map.diagrams.py
+++ docs/src/images/cluster-map.diagrams.py
@@ -1,11 +1,5 @@
 from diagrams import Cluster, Diagram, Edge
-from diagrams.generic.network import Router, Switch
-from diagrams.onprem.network import Internet
 from diagrams.onprem.compute import Server
-from diagrams.saas.cdn import Cloudflare
-from diagrams.saas.chat import Discord
-from diagrams.generic.os import Android, LinuxGeneral, Windows
-from diagrams.aws.iot import IotAlexaEcho, IotLightbulb, IotSensor
 
 with Diagram("Cluster Map", show=False):
     with Cluster("Technocore\nSidero master"):
--- docs/src/images/network-map.diagrams.py
+++ docs/src/images/network-map.diagrams.py
@@ -1,9 +1,6 @@
 from diagrams import Cluster, Diagram, Edge
 from diagrams.generic.network import Router, Switch
 from diagrams.onprem.network import Internet
-from diagrams.onprem.compute import Server
-from diagrams.saas.cdn import Cloudflare
-from diagrams.saas.chat import Discord
 from diagrams.generic.os import Android, LinuxGeneral, Windows
 from diagrams.aws.iot import IotAlexaEcho, IotLightbulb, IotSensor
 from diagrams.custom import Custom
