5f69a5702f1a6112e04ddd250c6e1fa1ed8bf092
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2022-09-12 20:16:37 +1000

    fix

--- k8s/manifests/core/system-monitoring/loki/provision/provision.sh
+++ k8s/manifests/core/system-monitoring/loki/provision/provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mc admin user add s3 loki $LOKI_S3_PASSWORD
-mc mb s3/loki
-mc admin policy add s3 loki-private loki-user-policy.json
-mc admin policy set s3 loki-private user=loki
+mc admin user add minio loki $LOKI_S3_PASSWORD
+mc mb minio/loki
+mc admin policy add minio loki-private loki-user-policy.json
+mc admin policy set minio loki-private user=loki
