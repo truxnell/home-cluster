0e4d0b42769fc5b7348c9447dcd428df3eb617b4
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-01-22 12:10:13 +1100

    fix: try new tool

--- tools/app-kustomize-gen.sh
+++ tools/app-kustomize-gen.sh
@@ -2,75 +2,106 @@
 shopt -s extglob
 
 ROOT=$(git rev-parse --show-toplevel)
-K8S_ROOT="$ROOT/kubernetes/apps"
+K8S_FOLDER="kubernetes/apps"
+K8S_ROOT="$ROOT/$K8S_FOLDER"
 
-for DIR in $K8S_ROOT/*/
-do
-  echo "Processing $DIR"
+for DIR in $K8S_ROOT/*/; do
+    echo "Processing $DIR"
 
-  if [ ! -f "$DIR/namespace.yaml" ]; then
+    ns=$(basename "$DIR")
+    export NAMESPACE=$ns
+    if [ ! -f "$DIR/namespace.yaml" ]; then
 
-      echo "$DIR missing namespace, creating"
+        echo "$DIR missing namespace, creating"
 
-      ns=$(basename $DIR)
-      export NAMESPACE=$ns
+        ns=$(basename $DIR)
+        export NAMESPACE=$ns
 
-      envsubst < "$ROOT/templates/namespace/namespace.yaml" > "$DIR/namespace.yaml"
-
-  fi
-
-  envsubst < "$ROOT/templates/namespace/kustomization.yaml" > "$DIR/kustomization.yaml"
-
-  for FILE in $DIR*.yaml
-  do
-
-    if [ ! $(basename $FILE) == "kustomization.yaml" ] && [ ! $(basename $FILE) == "namespace.yaml" ]; then
-      echo "  - ./$FILE" >>   "$DIR/kustomization.yaml"
-    fi
-
-  done
-
-  for APP in $DIR*/
-  do
-    APP_NAME=$(basename $APP)
-    echo " Adding $APP_NAME to $FILE"
-    if [ ! -f "$APP/ks.yaml" ]; then
-
-      echo "WARN:: $APP_NAME is missing its ks.yaml"
-
-    elif [ -f "$APP/wip" ]; then
-
-      echo "WARN:: $APP_NAME has a wip file, adding commented"
-      echo "  # - ./$APP_NAME/ks.yaml" >>   "$DIR/kustomization.yaml"
-
-    else
-
-      echo "  - ./$APP_NAME/ks.yaml" >>   "$DIR/kustomization.yaml"
+        envsubst <"$ROOT/templates/namespace/namespace.yaml" >"$DIR/namespace.yaml"
 
     fi
 
+    envsubst <"$ROOT/templates/namespace/kustomization.yaml" >"$DIR/kustomization.yaml"
 
-    appname=$(basename "$DIR")
-    ns=$(basename $(dirname "$DIR"))
+    for FILE in $DIR*.yaml; do
 
-    export FULLDIR="./$K8S_FOLDER/$ns/$APP_NAME"
-    export APP_NAME=$appname
-    envsubst < "$ROOT/templates/ks/ks.yaml" > "$dir/ks.yaml"
+        if [ ! $(basename $FILE) == "kustomization.yaml" ] && [ ! $(basename $FILE) == "namespace.yaml" ]; then
+            echo "  - ./$FILE" >>"$DIR/kustomization.yaml"
+        fi
 
-  # if [ ! -d "$dir/app" ]; then
-  #   echo "creating app folder for $dir"
-  #   mkdir "$dir/app"
-  #   mv $dir/!(app) "$dir/app"
-  #   appname=$(basename "$dir")
-  #   ns=$(basename $(dirname "$dir"))
-  #
-  #   export FULLDIR="$dir"
-  #   export APP_NAME=$appname
-  #   export NAMESPACE=$ns
-  #   envsubst < "$ROOT/templates/ks/ks.yaml" > "$dir/ks.yaml"
-  #
-  #
-  # fi
+    done
+
+    for APP in $DIR*/; do
+        APP_NAME=$(basename $APP)
+        echo " Adding $APP_NAME to $FILE"
+        if [ -f "$APP/wip" ]; then
+
+            echo "WARN:: $APP_NAME has a wip file, adding commented"
+            echo "  # - ./$APP_NAME/ks.yaml # TODO: Disabled by WIP file" >>"$DIR/kustomization.yaml"
+
+        else
+
+            rm "$APP/ks.yaml"
+            echo "  - ./$APP_NAME/ks.yaml" >>"$DIR/kustomization.yaml"
+
+        fi
+
+        # Check all yaml for correct namespace
+        for FILE in $APP*.yaml; do
+
+            if [[ $(yq '.metadata.namespace') != null ]]; then
+                echo "Ensuring namespace is $NAMESPACE in $FILE"
+                yq -i '.metadata.namespace=strenv(NAMESPACE)' "$FILE"
+
+            fi
+
+        done
+
+        for SECTION in $APP*/; do
+            SECTION_NAME=$(basename "$SECTION")
+
+            export FULLDIR="./$K8S_FOLDER/$ns/$APP_NAME/$SECTION_NAME"
+            export APP_NAME=$APP_NAME
+            export SECTION_NAME=$SECTION_NAME
+
+            echo "Adding $SECTION_NAME to $APP_NAME ks.yml"
+            envsubst <"$ROOT/templates/ks/ks.yaml" >>"$APP/ks.yaml"
+
+            #    if helmrelease present, add HR healthcheck and ensure values are aligned
+            if [ -f "$SECTION/helmrelease.yaml" ]; then
+
+                envsubst <"$ROOT/templates/ks/hr-add.yaml" >>"$APP/ks.yaml"
+                yq -i '.metadata.namespace=strenv(NAMESPACE)' "$SECTION/helmrelease.yaml"
+                yq -i '.metadata.name="cluster-app-"+strenv(APP_NAME)+"-"+strenv(SECTION_NAME)' "$SECTION/helmrelease.yaml"
+
+            fi
+
+            # Check all yaml for correct namespace
+            for FILE in $SECTION*.yaml; do
+
+                if [[ $(yq '.metadata.namespace') != null ]]; then
+                    echo "sdfEnsuring namespace is $NAMESPACE in $FILE"
+                    yq -i '.metadata.namespace=strenv(NAMESPACE)' "$FILE"
+
+                fi
+
+            done
+        done
+
+        # if [ ! -d "$dir/app" ]; then
+        #   echo "creating app folder for $dir"
+        #   mkdir "$dir/app"
+        #   mv $dir/!(app) "$dir/app"
+        #   appname=$(basename "$dir")
+        #   ns=$(basename $(dirname "$dir"))
+        #
+        #   export FULLDIR="$dir"
+        #   export APP_NAME=$appname
+        #   export NAMESPACE=$ns
+        #   envsubst < "$ROOT/templates/ks/ks.yaml" > "$dir/ks.yaml"
+        #
+        #
+        # fi
+    done
+
 done
-
-done;
--- tools/helm-install.sh
+++ tools/helm-install.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-
-
 entry() {
 
     echo "Performing $OPERATION of helm chart $SOURCE/$CHART named $NAME, version $VERSION into namespace 'testing' using values from $FILE"
     case "$OPERATION" in
-        install)
-            yq -r  .spec.values $FILE  | helm install $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
-            ;;
-        upgrade)
-            yq -r  .spec.values $FILE  | helm upgrade $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
-            ;;
-        delete)
-            helm uninstall $NAME -n testing
-            ;;
-        template)
-            yq -r  .spec.values $FILE  | helm template $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
-            ;;
+    install)
+        yq -r .spec.values $FILE | helm install $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
+        ;;
+    upgrade)
+        yq -r .spec.values $FILE | helm upgrade $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
+        ;;
+    delete)
+        helm uninstall $NAME -n testing
+        ;;
+    template)
+        yq -r .spec.values $FILE | helm template $NAME $SOURCE/$CHART --version $VERSION -n testing -f -
+        ;;
     esac
-
-
 
 }
 
 show_help() {
-cat << EOF
+    cat <<EOF
 Usage: $(basename "$0") <command> <file>
     -i or --install   - install helm to cluster with file's values
     -t or --template  - render helm template with specified files values
@@ -37,63 +33,59 @@ parse_command_line() {
 
     while :; do
         case "${1:-}" in
-            -h|--help)
+        -h | --help)
+            show_help
+            exit 1
+            ;;
+        -f | --file)
+            if [[ -n "${2:-}" ]]; then
+                FILE="$2"
+                shift
+            else
+                echo "ERROR: '-f|--file' cannot be empty." >&2
                 show_help
                 exit 1
-                ;;
-            -f|--file)
-                if [[ -n "${2:-}" ]]; then
-                    FILE="$2"
-                    shift
-                else
-                    echo "ERROR: '-f|--file' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -i|--install)
-                OPERATION="install"
-                ;;
-            -u|--upgrade)
-                OPERATION="upgrade"
-                ;;
-            -t|--template)
-                OPERATION="template"
-                ;;
-            -d|--delete)
-                OPERATION="delete"
-                ;;
-            -*|--*)
-                echo "ERROR: Unknown option $1"
-                show_help
-                exit 1
-                ;;
-            *)
-                break
-                ;;
+            fi
+            ;;
+        -i | --install)
+            OPERATION="install"
+            ;;
+        -u | --upgrade)
+            OPERATION="upgrade"
+            ;;
+        -t | --template)
+            OPERATION="template"
+            ;;
+        -d | --delete)
+            OPERATION="delete"
+            ;;
+        -* | --*)
+            echo "ERROR: Unknown option $1"
+            show_help
+            exit 1
+            ;;
+        *)
+            break
+            ;;
         esac
         shift
     done
 
-    if [ -z $OPERATION ]
-        then
+    if [ -z $OPERATION ]; then
         echo "ERROR: no operation requested (-t / -i / -d)"
         show_help
         exit 1
     fi
 
-    if [ -z "$FILE" ]
-        then
+    if [ -z "$FILE" ]; then
         echo "ERROR: '-f|--file' cannot be empty."
         show_help
         exit 1
     fi
 
-
     FILENAME=$(basename "$FILE")
 
-    if [ "$FILENAME" != "helmrelease.yaml" ]
-        then
+    if [ "$FILENAME" != "helmrelease.yaml" ]; then
         echo "ERROR: File '$FILENAME' is not 'helmrelase.yaml', exiting"
         show_help
         exit 1
