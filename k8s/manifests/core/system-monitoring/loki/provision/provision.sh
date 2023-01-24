4f191f17be81c9989141b9dc253bcaae800a3c65
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-01-24 21:42:09 +1100

    fix: delete old files

--- k8s/manifests/core/system-monitoring/loki/provision/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mc admin user add minio loki $LOKI_S3_PASSWORD
-mc mb minio/loki
-mc admin policy add minio loki-private loki-user-policy.json
-mc admin policy set minio loki-private user=loki
--- k8s/manifests/user/services/pleroma-rebased/config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :pleroma, configurable_from_database: true
-
-config :pleroma, Pleroma.Web.Endpoint,
-  url: [host: "fedi.voltaicforge.com"]
-
-
-config :pleroma, Pleroma.Web.WebFinger, domain: "voltaicforge.com"
