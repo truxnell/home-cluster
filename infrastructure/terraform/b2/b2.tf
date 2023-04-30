8b065186556c5971cfff643a19084bf393362d8c
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-05-01 08:42:19 +1000

    fix: add volsync lifecycle days

--- infrastructure/terraform/b2/b2.tf
+++ infrastructure/terraform/b2/b2.tf
@@ -13,6 +13,18 @@ resource "b2_bucket" "trux-volsync-backup-A73B8D" {
 
 }
 
+resource "b2_bucket_lifecycle_configuration" "trux-volsync-backup-A73B8D" {
+  bucket_id = b2_bucket.trux-volsync-backup-A73B8D.id
+
+  rule {
+    id      = "delete-versions-older-than-30-days"
+    status  = "enabled"
+    prefix  = ""
+    days    = 30
+    actions = ["delete"]
+  }
+}
+
 resource "b2_application_key" "nas-restic" {
   key_name  = "nas-restic"
   capabilities = [
