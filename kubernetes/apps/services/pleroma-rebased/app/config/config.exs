426902e88a2f282a2f23c8b6b9dfb7359c489f9e
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-06-12 09:31:13 +1000

    fix: move soapbox to new domain?

--- kubernetes/apps/services/pleroma-rebased/app/config/config.exs
+++ kubernetes/apps/services/pleroma-rebased/app/config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :pleroma, configurable_from_database: true
 
 config :pleroma, Pleroma.Web.Endpoint,
-  url: [host: "fedi.voltaicforge.com"]
+  url: [host: "fedi.truxnell.net"]
 
 
-config :pleroma, Pleroma.Web.WebFinger, domain: "voltaicforge.com"
+config :pleroma, Pleroma.Web.WebFinger, domain: "truxnell.net"
