7bb5d8278e5a6d349b6455b44d58c93b1c6f175e
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-08-06 09:38:00 +1000

    feat: move *arr to nas

--- kubernetes/helios/bootstrap/secrets.sh
+++ kubernetes/helios/bootstrap/secrets.sh
@@ -3,6 +3,8 @@
 # Didnt feel like adding external-secrets to my NAS cluster so Im just mananging secrets 'manually'
 kubectl create secret kopia-to-b2 -n default --from-env-file <$(doppler secrets download -p tf-b2 -c prd --no-file --format docker)
 kubectl create secret generic sonarr -n default --from-env-file=<(doppler secrets download -p sonarr -c prd --no-file --format docker)
+kubectl create secret generic radarr -n default --from-env-file=<(doppler secrets download -p radarr -c prd --no-file --format docker)
 kubectl create secret generic lidarr -n default --from-env-file=<(doppler secrets download -p lidarr -c prd --no-file --format docker)
+kubectl create secret generic readarr -n default --from-env-file=<(doppler secrets download -p readarr -c prd --no-file --format docker)
 
 kubectl create secret generic -n cert-manager cloudflare-api-token-secret --from-env-file <(doppler secrets download -p cert-manager -c prd --no-file --format docker)
