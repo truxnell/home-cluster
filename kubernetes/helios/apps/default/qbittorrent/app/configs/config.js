50d845f14b96a2421ad88f52e4580c54ace25d89
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-08-10 23:09:27 +1000

    fix: align to my prowlarr

--- kubernetes/helios/apps/default/qbittorrent/app/configs/config.js
+++ kubernetes/helios/apps/default/qbittorrent/app/configs/config.js
@@ -8,17 +8,17 @@ module.exports = {
   qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
-    "http://prowlarr.trux.dev/3/api?apikey={{ .PROWLARR__API_KEY }}", // ipt
-    "http://prowlarr.trux.dev/4/api?apikey={{ .PROWLARR__API_KEY }}", // tl
-    "http://prowlarr.trux.dev/5/api?apikey={{ .PROWLARR__API_KEY }}", // hdt
-    "http://prowlarr.trux.dev/6/api?apikey={{ .PROWLARR__API_KEY }}", // ts
-    "http://prowlarr.trux.dev/7/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
-    "http://prowlarr.trux.dev/8/api?apikey={{ .PROWLARR__API_KEY }}", // uhd
-    "http://prowlarr.trux.dev/11/api?apikey={{ .PROWLARR__API_KEY }}", // fl
-    "http://prowlarr.trux.dev/15/api?apikey={{ .PROWLARR__API_KEY }}", // st
-    "http://prowlarr.trux.dev/16/api?apikey={{ .PROWLARR__API_KEY }}", // blu
-    "http://prowlarr.trux.dev/17/api?apikey={{ .PROWLARR__API_KEY }}", // ptp
-    "http://prowlarr.trux.dev/42/api?apikey={{ .PROWLARR__API_KEY }}", // btn
+    "http://prowlarr.trux.dev/18/api?apikey={{ .PROWLARR__API_KEY }}", // ipt
+    "http://prowlarr.trux.dev/17/api?apikey={{ .PROWLARR__API_KEY }}", // tl
+    // "http://prowlarr.trux.dev/5/api?apikey={{ .PROWLARR__API_KEY }}", // hdt
+    // "http://prowlarr.trux.dev/6/api?apikey={{ .PROWLARR__API_KEY }}", // ts
+    // "http://prowlarr.trux.dev/7/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
+    // "http://prowlarr.trux.dev/8/api?apikey={{ .PROWLARR__API_KEY }}", // uhd
+    "http://prowlarr.trux.dev/19/api?apikey={{ .PROWLARR__API_KEY }}", // fl
+    // "http://prowlarr.trux.dev/15/api?apikey={{ .PROWLARR__API_KEY }}", // st
+    // "http://prowlarr.trux.dev/16/api?apikey={{ .PROWLARR__API_KEY }}", // blu
+    // "http://prowlarr.trux.dev/17/api?apikey={{ .PROWLARR__API_KEY }}", // ptp
+    // "http://prowlarr.trux.dev/42/api?apikey={{ .PROWLARR__API_KEY }}", // btn
   ],
 
   action: "inject",
