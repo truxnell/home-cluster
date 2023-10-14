65804febc8bf3ff81e7be9bd4733339e492fa64c
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-10-14 18:29:18 +1100

    fix: split out xseed

--- kubernetes/helios/apps/default/cross-seed/app/configs/config.js
+++ kubernetes/helios/apps/default/cross-seed/app/configs/config.js
@@ -8,30 +8,32 @@ module.exports = {
   qbittorrentUrl: "http://localhost:8080",
 
   torznab: [
-    "https://prowlarr.trux.dev/18/api?apikey={{ .PROWLARR__API_KEY }}", // ipt
+    // "https://prowlarr.trux.dev/18/api?apikey={{ .PROWLARR__API_KEY }}", // ipt
     "https://prowlarr.trux.dev/17/api?apikey={{ .PROWLARR__API_KEY }}", // tl
-    "https://prowlarr.trux.dev/27/api?apikey={{ .PROWLARR__API_KEY }}", // hdt
-    // "https://prowlarr.trux.dev/6/api?apikey={{ .PROWLARR__API_KEY }}", // ts
-    // "https://prowlarr.trux.dev/7/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
-    "https://prowlarr.trux.dev/40/api?apikey={{ .PROWLARR__API_KEY }}", // uhd
     "https://prowlarr.trux.dev/19/api?apikey={{ .PROWLARR__API_KEY }}", // fl
     "https://prowlarr.trux.dev/30/api?apikey={{ .PROWLARR__API_KEY }}", // st
-    "https://prowlarr.trux.dev/24/api?apikey={{ .PROWLARR__API_KEY }}", // blu
     "https://prowlarr.trux.dev/28/api?apikey={{ .PROWLARR__API_KEY }}", // hds
-    // "https://prowlarr.trux.dev/17/api?apikey={{ .PROWLARR__API_KEY }}", // ptp
-    // "https://prowlarr.trux.dev/42/api?apikey={{ .PROWLARR__API_KEY }}", // btn
-    "https://prowlarr.trux.dev/28/api?apikey={{ .PROWLARR__API_KEY }}", // hds
-    "https://prowlarr.trux.dev/33/api?apikey={{ .PROWLARR__API_KEY }}", // sbs
-    "https://prowlarr.trux.dev/34/api?apikey={{ .PROWLARR__API_KEY }}", // phd
-    "https://prowlarr.trux.dev/39/api?apikey={{ .PROWLARR__API_KEY }}", // ath
     "https://prowlarr.trux.dev/41/api?apikey={{ .PROWLARR__API_KEY }}", // rtt
-    "https://prowlarr.trux.dev/42/api?apikey={{ .PROWLARR__API_KEY }}", // stc
-    "https://prowlarr.trux.dev/44/api?apikey={{ .PROWLARR__API_KEY }}", // stt
+
+    "https://prowlarr.trux.dev/27/api?apikey={{ .PROWLARR__API_KEY }}", // hdt
+    "https://prowlarr.trux.dev/40/api?apikey={{ .PROWLARR__API_KEY }}", // uhd
+    "https://prowlarr.trux.dev/24/api?apikey={{ .PROWLARR__API_KEY }}", // blu
     "https://prowlarr.trux.dev/43/api?apikey={{ .PROWLARR__API_KEY }}", // ath
+
+    "https://prowlarr.trux.dev/33/api?apikey={{ .PROWLARR__API_KEY }}", // sbs
+    "https://prowlarr.trux.dev/46/api?apikey={{ .PROWLARR__API_KEY }}", // dcc
+
+    "https://prowlarr.trux.dev/34/api?apikey={{ .PROWLARR__API_KEY }}", // phd
+    "https://prowlarr.trux.dev/35/api?apikey={{ .PROWLARR__API_KEY }}", // cz
+    "https://prowlarr.trux.dev/36/api?apikey={{ .PROWLARR__API_KEY }}", // az
+
+    "https://prowlarr.trux.dev/39/api?apikey={{ .PROWLARR__API_KEY }}", // ant
+    "https://prowlarr.trux.dev/45/api?apikey={{ .PROWLARR__API_KEY }}", // nbl
   ],
 
   action: "inject",
   includeEpisodes: true,
+  includeSingleEpisodes: true,
   includeNonVideos: true,
   duplicateCategories: true,
   matchMode: "safe",
@@ -48,6 +50,7 @@ module.exports = {
     "/media/downloads/qbittorrent/complete/sonarr/",
   ],
 
+  maxDataDepth: 1,
   outputDir: "/config/xseeds",
   torrentDir: "/config/qBittorrent/BT_backup",
 };
